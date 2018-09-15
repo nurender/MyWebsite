4d624bc1470610cd61c2cafbd78642f57eeaf072
nurender <nurenderbishnoi@gmail.com>
2018-09-15 15:46:42 +0530

    9/15/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-import  { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HTMLComponent } from './html/html.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FOOTERComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { ErrorComponent } from './error/error.component';
+import { text } from '@angular/core/src/render3/instructions';
 
 
 let routes : Routes = [
   {
-    path : '',
-    component : HTMLComponent
+    path : 'html',
+    component : BodyComponent,
+    children:[{
+      path: 'html',
+      component : BodyComponent
+    },
+    {
+      path : 'HtmlHome',
+      component : TestComponent
+    },
+    {
+      path : 'Agnes',
+      component : TestComponent
+    },
+    {
+      path : "Nurender",
+      component : HTMLComponent
+    }]
+  },
+  {
+    path : 'css',
+    component : BodyComponent
+  },
+  {
+    path : 'javascript',
+    component : BodyComponent
+  },
+  {
+    path : 'bootstrap',
+    component : BodyComponent
+  },
+  {
+    path : 'jquery',
+    component : BodyComponent
+  },
+  {
+    path : 'angular',
+    component : BodyComponent
+  },
+  {
+    path : 'java',
+    component : BodyComponent
   },
   {
     path : 'text',
     component : TestComponent
+  },
+  {
+    path: '**',
+    component : ErrorComponent          
   }
 ];
 @NgModule({
@@ -25,10 +73,13 @@ let routes : Routes = [
     TestComponent,
     HTMLComponent,
     SidebarComponent,
-    FOOTERComponent
+    FOOTERComponent,
+    BodyComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router : Router) { }
 
   ngOnInit() {
   }
@@ -38,4 +39,11 @@ myFunction() {
       }
   }
 }
+
+routerLink(){ 
+    console.log("hello sir ji kese ho ji kya kar rhe ho ji");
+    this.router.navigate(['/Nurender'])
+    
+}
+
 }
